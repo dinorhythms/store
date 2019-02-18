15a147c38987ead684c7432d02840b2de9df1fae
Olanrewaju Kazeem <OlanrewajuKazeem@CDKINDUSTRIES.COM>
2019-02-18 16:49:54 +0100

    Context Api

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react'
 
 export default class Product extends Component {
   render() {
+    const {id, title, img, price, inCart} = this.props.product;
     return (
-      <div>
-        <h3>Hello fro Product</h3>
+      <div className="col-9 mx-auto col-md-6 col-lg-3">
+        <div className="card text-left">
+          <div className="img-thumbnail p-5"><img className="card-img-top" src={img} alt=""/></div>
+          <div className="card-body text-center">
+            <h4 className="card-title">{title}</h4>
+            <p className="card-text">{price}</p>
+          </div>
+        </div>
       </div>
     )
   }
