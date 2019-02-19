c22c25e4863a28d86766d7aa71784da8f3068525
Olanrewaju Kazeem <OlanrewajuKazeem@CDKINDUSTRIES.COM>
2019-02-19 10:41:37 +0100

    Add some margin-bottom to the products cards

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,7 @@ export default class Product extends Component {
   render() {
     const {id, title, img, price, inCart} = this.props.product;
     return (
-      <div className="col-9 mx-auto col-md-6 col-lg-3">
+      <div className="col-9 mx-auto col-md-6 col-lg-3 mb-4">
         <div className="card text-left">
           <div className="img-thumbnail p-5"><img className="card-img-top" src={img} alt=""/></div>
           <div className="card-body text-center">
