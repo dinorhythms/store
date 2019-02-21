13a632eea18ab1c1073b5fc6502dc9519a7a0a76
Olanrewaju Kazeem <OlanrewajuKazeem@CDKINDUSTRIES.COM>
2019-02-21 15:52:11 +0100

    Integrating Add to Cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 import './product.css';
+import { ProductConsumer } from '../context';
 
 export default class Product extends Component {
   render() {
     const {id, title, img, price, inCart} = this.props.product;
     return (
-      <div className="col-9 mx-auto col-md-6 col-lg-3 mb-4">
-        <div className="card text-left">
-          <div className="img-container img-thumbnail p-5"
-          onClick={()=>console.log('You clicked the image container')}
-          >
-            <Link to="/details">
-              <img className="card-img-top" src={img} alt=""/>
-            </Link>
-            <button type="button" className="cart-btn btn btn-primary" 
-            disabled={inCart}
-            onClick={()=>console.log('You added to the cat')}
-            >
-              {inCart?(
-                <span>In Cart</span>
-              ):(
-                <i className="fa fa-cart-plus" aria-hidden="true"></i>
-              )}
-            </button>
-          </div>
-          <div className="card-body d-flex justify-content-between text-center">
-            <p className="card-text mr-auto">{title}</p>
-            <h4 className="card-title">$ {price}</h4>
-          </div>
-        </div>
-      </div>
+      <ProductConsumer>
+        {value=>{
+          const {handleDetail} = value;
+          return(
+            <div className="col-9 mx-auto col-md-6 col-lg-3 mb-4">
+              <div className="card text-left">
+                <div className="img-container img-thumbnail p-5"
+                onClick={()=>handleDetail(id)}
+                >
+                  <Link to="/details">
+                    <img className="card-img-top" src={img} alt=""/>
+                  </Link>
+                  <button type="button" className="cart-btn btn btn-primary" 
+                  disabled={inCart}
+                  onClick={()=>value.addToCart(id)}
+                  >
+                    {inCart?(
+                      <span>In Cart</span>
+                    ):(
+                      <i className="fa fa-cart-plus" aria-hidden="true"></i>
+                    )}
+                  </button>
+                </div>
+                <div className="card-body d-flex justify-content-between text-center">
+                  <p className="card-text mr-auto">{title}</p>
+                  <h4 className="card-title">$ {price}</h4>
+                </div>
+              </div>
+            </div>
+          )
+        }}
+      </ProductConsumer>
+      
     )
   }
 }
--- src/context.js
+++ src/context.js
@@ -7,7 +7,8 @@ export default class ProductProvider extends Component {
 
   state = {
       products:[],
-      detailProduct
+      detailProduct,
+      cart:[]
   }
 
   componentDidMount(){
@@ -25,12 +26,30 @@ export default class ProductProvider extends Component {
     })
   }
 
-  handleDetail = () =>{
-      console.log("hello from detail")
+  handleDetail = (id) =>{
+      const detailProduct = this.state.products.filter(product => product.id === id);
+      this.setState({detailProduct: detailProduct[0]});
   }
 
   addToCart = (id) =>{
-      console.log("hello from addToCart", id)
+      // console.log("hello from addToCart", id)
+      let tempProducts = [...this.state.products];
+      let product = tempProducts.filter(product=>product.id === id);
+      product = product[0];
+      product.inCart = true;
+      product.count = 1;
+      product.total = product.price;
+      // console.log(product)
+      const cart = [...this.state.cart, product];
+      this.setState(()=>{
+        return {
+        products: tempProducts,
+        cart: [...this.state.cart, product]
+        }
+      },()=>{
+        console.log(this.state);
+      });
+      
   }
 
   render() {
